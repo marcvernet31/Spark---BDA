868a208ce7059a1d85dc93448efefe4f22ec0fff
marcvernet31 <44305244+marcvernet31@users.noreply.github.com>
2020-12-13 16:47:04 +0100

    Add files via upload

--- management.py
+++ management.py
@@ -4,7 +4,9 @@ from pyspark.sql import SQLContext
 from pyspark.sql import Row
 from pyspark.sql import SparkSession
 
+import datetime
 from os import walk
+from pyspark.sql.functions import datediff, to_date, lit, unix_timestamp,col
 
 
 from password import *
@@ -12,6 +14,9 @@ from password import *
 Cal tenir el fitxer password.py and:
     AIMSusername = "nom.cognom"
     AIMSpassword = "DBddmmyy"
+
+(linux) Cal posar:
+     f5fpc -s -x -t https://upclink.upc.edu
 """
 
 
@@ -49,7 +54,16 @@ Suposicions generals:
     (es pot comprovar facil)
     - Per calcular DM, suposem que un vol canelat no conta com a retard
     - Considerem actualdeparture el dia en que es fa el vol (per a l'hora d'agregar en dies)
+    - Suposem que tots els vols arriben despres de la sortida (actualarrival > actualdeparture) (LOL)
+    - Per FC assumim que totes les avions que despeguen aterren en algun moment
+    - Un FC conta pel dia en que despega, encara que aterri en un altre dia
+    - Assumim flightid un identificador únic
+    - Si un vol arriba abans de la scheduledarrival té retard negatiu que resta al retard total
+        (esta be ?)
 """
+
+
+
 def process(sc):
     sess = SparkSession(sc)
 
@@ -74,38 +88,79 @@ def process(sc):
     # Afegir la columna day (2012-7-23), per poder agregar per dia
     # Eliminar vols cancelats
     AIMS = (AIMS.withColumn('day', AIMS['actualarrival'].cast('date'))
-        .filter(AIMS['cancelled'] == "False"))
+        .filter(AIMS['cancelled'] == "False").withColumn("duration", datediff(AIMS['actualdeparture'], AIMS['actualarrival']))
+        )
 
-    # Seleccionar: aircraftid, day, (actualarrival-actualdeparture)
-    # FH = AIMS.rdd.map(lambda t: (t[0],t[14], t[8]-t[7]))
-    # FC =
-    # DM =
 
-    # Lectura dels csv
+    # Sortirda: day(datetime), aircraftregistration(string), sum(duration_hours)
+    #   duration_hours = (actualarrival-actualdeparture) en hores (per dia)
+    # FUNCIONA!
+    FH = (AIMS.withColumn('duration_hours',
+            (unix_timestamp(AIMS['actualarrival'], "yyyy-MM-dd'T'hh:mm:ss")
+            - unix_timestamp(AIMS['actualdeparture'], "yyyy-MM-dd'T'hh:mm:ss"))/(60*60)
+        )
+        .select("day", "aircraftregistration", "duration_hours")
+        .groupBy("day","aircraftregistration").sum("duration_hours")
+        .withColumnRenamed("sum(duration_hours)", "FH")
+        )
+
+    # Sortirda: day(datetime), aircraftregistration(string), count()
+    #  FUNCIONA!
+    FC = (AIMS.select("day", "aircraftregistration", "flightid")
+        .groupBy("day","aircraftregistration").count()
+         .withColumnRenamed("count", "FC")
+    )
+
+    # Sortirda: day(datetime), aircraftregistration(string), sum(delay_hours)
+    #   delay_hours = (actualarrival-scheduledarrival) en hores (per dia)
+    # FUNCIONA!
+    DM = (AIMS.withColumn('delay_hours',
+            (unix_timestamp(AIMS['actualarrival'], "yyyy-MM-dd'T'hh:mm:ss")
+            - unix_timestamp(AIMS['scheduledarrival'], "yyyy-MM-dd'T'hh:mm:ss"))/(60*60)
+        )
+        .select("day", "aircraftregistration", "delay_hours")
+        .groupBy("day","aircraftregistration").sum("delay_hours")
+        .withColumnRenamed("sum(delay_hours)", "DM")
+    )
+
+    # Llista de tots els .csv a llegir
     path = "resources/trainingData"
     f = []
     for (dirpath, dirnames, filenames) in walk(path):
         f.extend(filenames)
         break
 
-    # Crear un dataFrame on posar les dades del csv (quin format ?)
+    # Extreure dades de tots els .csv en un dataframe
+    # Sortida: day(datetime), aircraftregistration(string), sensor_avg
+    # FUNCIONA!
+    vals = list()
+    for filename in f:
+        # Model de l'avio
+        division = filename.split("-")
+        aircraftid = (division[4] + "-" + division[5]).split(".")[0]
+
+        input = (sc.textFile("./" + path + "/" + filename)
+        	.filter(lambda t: "date" not in t))
+
+        # Mitjana del valor del sensor
+        sensors = input.map(lambda t: t.split(";")[2]).collect()
+        sensors = list(map(float, sensors))
+        sensor_avg = sum(sensors) / len(sensors)
+
+        # Data
+        date = list(input.map(lambda t: t.split(";")[0]).collect())
+        date = date[1].split(" ")[0] # Seleccionar una data i eliminar h:m:s
+
+        # Posar tot al DataFrame
+        vals.append([date, aircraftid, sensor_avg])
+
+    columns = ['day', 'aircraftregistration', 'sensor_avg']
+    SensorLectures = sess.createDataFrame(vals, columns)
+
+    # Crear labels de manteniment
     # ...
 
-    for filename in f:
-        # Mitjana del valor del sensor
-        input = (sc.textFile("./" + path + "/" + filename)
-        		.filter(lambda t: "date" not in t)
-                .map(lambda t: t.split(";")[2]).collect())
-        input = list(map(float, input))
-        sensor_avg = sum(input) / len(input)
-
-        # Trobar el aircraftid i dia (separar del titol, ddmmyy to dateTime,...)
-        # ...
-        # Posar tot al DataFrame
-        # ...
-
-
-    # Join de KPI's derivats de AIMS amb dataFrame dels csv
+    # Join de tots els dataframes de AMIS i csv
     # ...
 
     # Extreure led dades de manteniment de AMOS i creuar amb la taula anterior
